dae1d1453689c772dded0b90f6305ac22f2a9d55
Edson Jordan <edson_4555@hotmail.com>
2020-07-31 01:21:56 -0500

    agregando archivos js y pagination foot

---
+++ pagination.js
@@ -0,0 +1,32 @@
+class Pagination{
+    constructor(){
+        
+    }
+    get_id(node){
+        return document.getElementById(node);        
+    }
+
+    /* Add nodechild */
+    add_childnode_radio_paginatio(node, array, child, attibute, type, name){
+    const fragment = document.createDocumentFragment()
+        for(const date of array){
+            const nodechild = document.createElement(child);
+            const nodelabel = document.createElement('label');
+/*AGREGANDO ATRIBUTOS A RADIO */            
+            nodechild.setAttribute(attibute, date);            
+            nodechild.setAttribute('type', type);
+            nodechild.setAttribute('name', name);
+            nodechild.classList.add('radio-pagination-foot')
+/* AGREGANDO ATRIBUTOS A LABEL */
+            nodechild.setAttribute('id', date);
+            nodelabel.setAttribute('for', date)
+            nodelabel.classList.add('label-pagination')
+            nodelabel.textContent = date;
+            fragment.appendChild(nodelabel)
+            fragment.appendChild(nodechild)
+        }
+        return node.appendChild(fragment);
+    }
+
+
+}
--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-const form = document.getElementById('form');
-form.addEventListener('submit', (e) => {
+/* Herencia Pagination */
+const class_pagination = new Pagination();
+/* Formulario get id */
+const form_pagination = class_pagination.get_id('foot_form_pagination');
+/* Get id foot */
+
+const array_pagination = [1, 2, 3, 4, 5, 6];
+
+/* form.addEventListener('submit', (e) => {
     e.preventDefault();
 })
+ */
+class_pagination.add_childnode_radio_paginatio(form_pagination, array_pagination, "input", 'value', 'radio', 'group-pagination');
+//console.log(form)
