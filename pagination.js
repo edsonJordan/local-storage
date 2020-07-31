cd5b2104e70d2da54b793189d0d458204a6497fb
Edson Jordan <edson_4555@hotmail.com>
2020-07-31 05:13:43 -0500

    terminando la amanecida

--- pagination.js
+++ pagination.js
@@ -1,5 +1,5 @@
-class Pagination{
-    constructor(){
+export class Pagination
+{    constructor(){
         
     }
     get_id(node){
@@ -9,7 +9,7 @@ class Pagination{
     /* Add nodechild */
     add_childnode_radio_paginatio(node, array, child, attibute, type, name){
     const fragment = document.createDocumentFragment()
-        for(const date of array){
+        for(const date of array){        
             const nodechild = document.createElement(child);
             const nodelabel = document.createElement('label');
 /*AGREGANDO ATRIBUTOS A RADIO */            
@@ -17,8 +17,8 @@ class Pagination{
             nodechild.setAttribute('type', type);
             nodechild.setAttribute('name', name);
             nodechild.classList.add('radio-pagination-foot')
-/* AGREGANDO ATRIBUTOS A LABEL */
             nodechild.setAttribute('id', date);
+/* AGREGANDO ATRIBUTOS A LABEL */            
             nodelabel.setAttribute('for', date)
             nodelabel.classList.add('label-pagination-foot')
             nodelabel.textContent = date;
@@ -27,6 +27,12 @@ class Pagination{
         }
         return node.appendChild(fragment);
     }
-
-
+    clicklistenerinput(node){
+    node.addEventListener('click', (e) => {
+         const target = e.target
+            if(target.nodeName === 'INPUT'){
+             return console.log(target);             
+            }                        
+        } )        
+    }
 }
--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 /* Herencia Pagination */
+import {Pagination} from "/pagination.js";
+
 const class_pagination = new Pagination();
 /* Formulario get id */
 const form_pagination = class_pagination.get_id('foot_form_pagination');
-/* Get id foot */
-
+/* Get id pagination first */
 const array_pagination = [1, 2, 3, 4, 5, 6];
-
 /* form.addEventListener('submit', (e) => {
     e.preventDefault();
 })
  */
 class_pagination.add_childnode_radio_paginatio(form_pagination, array_pagination, "input", 'value', 'radio', 'group-pagination');
-//console.log(form)
+const pagina = class_pagination.clicklistenerinput(form_pagination);
+const first_pagination = class_pagination.get_id('1');
+/* first_pagination.setAttibute("checked", true); */
+/* " checked="true"  */
+first_pagination.setAttribute("checked", true);
+first_pagination.classList.add('active')
+console.log(first_pagination);
