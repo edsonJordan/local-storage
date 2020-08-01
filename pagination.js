0ee939febf28dbd27b9a0ed3784ebecc60741bf2
Edson Jordan <edson_4555@hotmail.com>
2020-08-01 02:16:30 -0500

    agregando git pagination

--- pagination.js
+++ pagination.js
@@ -1,38 +1,65 @@
 export class Pagination
-{    constructor(){
-        
+{ 
+    
+    constructor(pagination, seudopaginate){
+    this.pagination = pagination
+    this.seudopaginate =seudopaginate
+    }
+    set_pagination(array = []){
+        this.pagination = array;
+    }
+    get_pagination(){
+        return this.pagination;
     }
     get_id(node){
         return document.getElementById(node);        
     }
-
     /* Add nodechild */
     add_childnode_radio_paginatio(node, array, child, attibute, type, name){
     const fragment = document.createDocumentFragment()
         for(const date of array){        
             const nodechild = document.createElement(child);
             const nodelabel = document.createElement('label');
-/*AGREGANDO ATRIBUTOS A RADIO */            
+            /* ADD ATTRIBUTE  LABEL */            
+            nodelabel.setAttribute('for', 'radio-pagination-foot-'+date)
+            nodelabel.classList.add('label-pagination-foot')
+            nodelabel.setAttribute('id', 'label-pagination-foot-'+date)
+            nodelabel.textContent = date;
+            /*ADD  ATTIBUTE  RADIO */            
             nodechild.setAttribute(attibute, date);            
             nodechild.setAttribute('type', type);
-            nodechild.setAttribute('name', name);
-            nodechild.classList.add('radio-pagination-foot')
-            nodechild.setAttribute('id', date);
-/* AGREGANDO ATRIBUTOS A LABEL */            
-            nodelabel.setAttribute('for', date)
-            nodelabel.classList.add('label-pagination-foot')
-            nodelabel.textContent = date;
-            fragment.appendChild(nodelabel)
+            nodechild.setAttribute('name', name);            
+            nodechild.classList.add('radio-pagination-foot')            
+            nodechild.setAttribute('id', 'radio-pagination-foot-'+date);
             fragment.appendChild(nodechild)
+            fragment.appendChild(nodelabel)
         }
         return node.appendChild(fragment);
     }
-    clicklistenerinput(node){
-    node.addEventListener('click', (e) => {
-         const target = e.target
-            if(target.nodeName === 'INPUT'){
-             return console.log(target);             
-            }                        
-        } )        
+    /*  
+    set_seudopaginate(before_radio, before_label, after_radio, after_label){
+        this.seudopaginate={before_radio : [before_radio], before_label : [before_label],
+             after_radio : [after_radio], after_label: [after_label]};               
     }
+    get_seudopaginate(){
+        return this.seudopaginate; 
+    }
+    action_seudopaginate(action = []){
+        console.log(action.before_radio, action.before_label,
+            action.after_radio, action.after_label  );                
+    }*/
+
+    clicklistenerinput(node){        
+        node.addEventListener('click', (e) => {                  
+            const targe  = e.target                    
+        if(targe.nodeName === 'INPUT'){            
+            this.set_pagination(targe.value)                                     
+            return console.log(this.get_pagination());   
+        }                               
+        
+                            
+        }  )    
+        
+    }
+
 }
--- script.js
+++ script.js
@@ -2,19 +2,51 @@
 import {Pagination} from "/pagination.js";
 
 const class_pagination = new Pagination();
-/* Formulario get id */
-const form_pagination = class_pagination.get_id('foot_form_pagination');
+/* Form get id from header */
+const form_pagination__header = class_pagination.get_id('header_form_pagination');
+/* Formulario get id from footer */
+const form_pagination__footer = class_pagination.get_id('foot_form_pagination');
 /* Get id pagination first */
-const array_pagination = [1, 2, 3, 4, 5, 6];
-/* form.addEventListener('submit', (e) => {
-    e.preventDefault();
-})
- */
-class_pagination.add_childnode_radio_paginatio(form_pagination, array_pagination, "input", 'value', 'radio', 'group-pagination');
-const pagina = class_pagination.clicklistenerinput(form_pagination);
-const first_pagination = class_pagination.get_id('1');
-/* first_pagination.setAttibute("checked", true); */
-/* " checked="true"  */
-first_pagination.setAttribute("checked", true);
-first_pagination.classList.add('active')
-console.log(first_pagination);
+const array_pagination = [1, 2, 3, 4, 5];
+/* add child nodes for form_pagination */
+class_pagination.add_childnode_radio_paginatio(form_pagination__footer, array_pagination, "input", 'value', 'radio', 'group-pagination');
+/* GET ID first radio and  add attribute check */
+const first_pagination = class_pagination.get_id('radio-pagination-foot-1');
+first_pagination.checked = true;
+/* Listener radio header */
+//const pagination_header = class_pagination.clicklistenerinput(form_pagination__header);
+//console.log(pagination_header);
+/* form_pagination__header.addEventListener('click', (e)=>{
+    console.log(e.target)
+}) */
+console.log(form_pagination__header.innethtml);
+
+//console.log(form_pagination__header);
+//console.log(form_pagination__footer);
+
+/* Listener radio footer */
+const pagination_footer = class_pagination.clicklistenerinput(form_pagination__footer);
+if(pagination_footer === undefined){
+     console.log(1);        
+}else{
+    console.log(pagination_footer);    
+}
+
+
+
+
+/* */
+/* Set pag option checked and label because required styles */
+//const pag_option = [{ radio :radios.id, label : radios.getAttribute('data-label') }, ];
+
+
+
+
+
+
+
+
+
+
+
+//console.log(first_pagination);
