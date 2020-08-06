84a55f70dff5079f13e7efbd78128ed36273783c
Edson Jordan <edson_4555@hotmail.com>
2020-08-06 18:01:10 -0500

    agregando patron singleton

--- scripts/activity.js
+++ scripts/activity.js
@@ -1,8 +1,55 @@
-export class Activity
-{
-    constructor(){
-    
+class Activity
+{   constructor(){            
+        this.actividad=null;
+        this.fecha = null;
     }
+    set setActivity(actividad){
+        this.actividad=actividad;
+    }
+    get getActivity(){
+        return this.actividad;
+    }
+    set setFecha(fecha){
+        this.fecha = fecha;
+    }    
+    get getFecha(){
+        return this.fecha;
+    }        
+}
+export class Builderactivity{
+    constructor(){     
+        this.setInstaBuilder()
+        
+        this.settingsObject = {
+            'background': '#fffff',
+            'version' : '2.1'
+        }
+        this.getInstaBuilder();
+    }
+    getInstaBuilder(){
+        this.activity= new Activity();
+        return Builderactivity.instance = this;
+    }
+    setInstaBuilder(){
+        if(Builderactivity.instance instanceof Builderactivity){
+            return Builderactivity.instance;
+        }
+    }
+    
 
-
+    /* get instActivity(){
+        this.activity = new Activity();
+    } */
+    setActivity(actividad){
+        this.activity.setActivity=actividad;
+        return this;
+    }
+    setFecha(fecha){
+        this.activity.setFecha = fecha;
+        return this;
+    }
+    build(){
+        return this.activity;
+    }
+    
 }
--- scripts/script-activity.js
+++ scripts/script-activity.js
@@ -1,4 +1,31 @@
-import {Activity} from "./activity.js";
+import {Builderactivity} from "./activity.js";
+/* const activity = new Builderactivity()
+                    .setActivity("trabajo")
+                    .setFecha("10/10/2020")
+                    .build() */;
+const a = new Builderactivity().setActivity("trabajo").build();
+const b = new Builderactivity().setActivity("paseo").build();
 
-const activity = new Activity()
+console.log(a);
+console.log(b);
+
+                    
+
+const form= document.getElementById("form");
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+    let actividad= document.getElementById("actividad");
+    let fecha= document.getElementById("fecha");            
+    if(fecha.value.length ==0 || actividad.value.length == 0){
+        console.log("uno de los datos esta vacio");        
+    }else{
+        localStorage.clear();
+        localStorage.setItem('hola', fecha.value)
+    }
+})
+
+
+
+
+
 
