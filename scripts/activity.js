8d9200b18cb84bc8e0e6a07253fe527b21c81a69
Edson Jordan <edson_4555@hotmail.com>
2020-08-08 23:37:55 -0500

    agregando funciones para recojer datos de formulario

--- scripts/activity.js
+++ scripts/activity.js
@@ -1,7 +1,34 @@
-class Activity
-{   constructor(){            
+class Activity{   
+    constructor(){            
         this.actividad=null;
-        this.fecha = null;
+        this.fecha = null;        
+        this.Dactivity=null;
+        this.Dfecha=null;
+        this.Inputs=[];
+    }
+   
+    set setDactivity(Dactivity){
+        this.Dactivity=Dactivity;
+    }
+    get getDactivity(){
+        return this.Dactivity;
+    }
+    set setInputform(data){
+        const form = document.getElementById(data);
+        for(const key of form){
+            if(key.id !== ""){
+                this.Inputs.push({[key.name]: key.id})
+            }
+        }     
+    }
+    get getInputform(){
+        return this.Inputs;
+    }
+    set setDfecha(Dfecha){
+        this.Dfecha=Dfecha;
+    }
+    get getDfecha(){
+        return this.Dfecha;
     }    
     set setActivity(actividad){
         this.actividad=actividad;
@@ -19,20 +46,37 @@ class Activity
 export class Builderactivity{
     constructor(){     
         this.activity = new Activity();
-    }      
+    }
+    setDactivity(Dactivity){
+        this.activity.setDactivity=document.getElementById(Dactivity);
+        return this;
+    }
+    setDfecha(Dfecha){
+        this.activity.setDfecha=document.getElementById(Dfecha);
+        return this;
+    }
     setActivity(actividad){
         this.activity.setActivity=actividad;
+        return this;
+    }
+    clearInput(data){        
+        for (const iterator of data) {            
+            document.getElementById(Object.values(iterator)[0]).value="";
+        }
+    }
+    setInputform(data){
+        this.activity.setInputform=data;
         return this;
     }
     setFecha(fecha){
         this.activity.setFecha = fecha;
         return this;
     }    
-     getLocal(key, data){
+    getLocal(key, data){
         localStorage.setItem(key, JSON.stringify(data));
-    return this;
-        
+    return this;        
     }
+    
     build(){
         return this.activity;
     }
--- scripts/script-activity.js
+++ scripts/script-activity.js
@@ -1,26 +1,47 @@
 import {Builderactivity} from "./activity.js";
-/* const activity = new Builderactivity()
+const activity = new Builderactivity()
                     .setActivity("trabajo")
                     .setFecha("10/10/2020")
-                    .build() */;
-const a = new Builderactivity().setActivity("trabajo").build();
-const b = new Builderactivity().setActivity("paseo").build();
-
-console.log(a);
-console.log(b);
-
-                    
-
+                    .build();                    
+const prueba = new Builderactivity()
+                    .setDactivity("actividad")
+                    .setDfecha("fecha")
+                    .setInputform("form")
+                    .build();                    
+/* console.log(activity.getActivity);
+console.log(activity.getFecha);*/                                  
 const form= document.getElementById("form");
+let data = []
+for (const key of form) {        
+    if(key.id !== ""){
+        //console.log(key.id);    
+        data.push({[key.name]:key.id})
+    }
+}
+for(const da of data){
+    //console.log(Object.values(da)[0]);    
+}
+//console.log(Object.values(data[0]));
+
 form.addEventListener('submit', (e)=> {
     e.preventDefault();
-    let actividad= document.getElementById("actividad");
-    let fecha= document.getElementById("fecha");            
-    if(fecha.value.length ==0 || actividad.value.length == 0){
-        console.log("uno de los datos esta vacio");        
+    let actividad= document.getElementById("actividad");    
+    let fecha= document.getElementById("fecha");        
+    
+                
+    if(fecha.value.length == 0 || actividad.value.length == 0){
+        console.log("Esta vacio uno de los 2 campos");        
     }else{
-        localStorage.clear();
-        localStorage.setItem('hola', fecha.value)
+    /*     activity.getActivity.value="";
+        activity.getFecha.value=""; */
+        const procedure = new Builderactivity().setFecha(fecha.value).setActivity(actividad.value).build();         
+        console.log(procedure);
+        
+        //console.log(procedure.getInputform());
+        console.log(prueba.Dfecha());        
+        //console.log(procedure.clearInput(procedure.getInputform()));
+        ;
+        //localStorage.setItem('hola', fecha.value) 
     }
 })
 
